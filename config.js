d2f82884454fb5573d744a8854a3fd48db39fba2
corden816 <816craft@gmail.com>
2024-10-26 00:36:01 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,4 @@
 const config = {
     apiKey: "fad4e222a3854bb99ed337f837a4e21c",
     region: "koreacentral"
-    /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://aka.ms; connect-src 'self' https://*.microsoft.com wss://*.speech.microsoft.com"
-          }
-        ]
-      }
-    ];
-  }
-};
-
-module.exports = nextConfig;
 };
